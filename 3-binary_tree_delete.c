8a0ef02e4c04d2f0bf21fee72f7eb0db1bde7431
davud <davudgeroqiq@gmail.com>
2026-01-30 12:00:24 +0000

    new commit

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,10 +7,13 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree->left != NULL)
-		binary_tree_delete(tree->left);
-	if (tree->right != NULL)
-		binary_tree_delete(tree->right);
-	free(tree->parent);
-	free(tree->n);
+	if (tree != NULL)
+	{
+		if (tree->left != NULL)
+			binary_tree_delete(tree->left);
+		if (tree->right != NULL)
+			binary_tree_delete(tree->right);
+		free(tree->parent);
+		free(tree->n);
+	}
 }
