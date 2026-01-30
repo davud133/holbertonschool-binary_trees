3c649c07b5dccedeb6fc0ecd316d8248c87f973f
davud <davudgeroqiq@gmail.com>
2026-01-30 10:00:03 +0000

    new commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,6 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left_node = binary_tree_node(parent, value);
-	binary_tree_t *parent_node = parent;
 
 	if (parent->left == NULL)
 	{
