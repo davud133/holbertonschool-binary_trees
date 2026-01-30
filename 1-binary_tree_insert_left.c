63f934f07813dae40699866e83d950446295fa77
davud <davudgeroqiq@gmail.com>
2026-01-30 09:59:18 +0000

    new commit

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts node to left node of another node
+ * @parent: parent node of a node
+ * @value: data of a node
+ *
+ * Return: pointer to new node or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_left_node = binary_tree_node(parent, value);
+	binary_tree_t *parent_node = parent;
+
+	if (parent->left == NULL)
+	{
+		parent->left = new_left_node;
+	}
+	else
+	{
+		parent->left->parent = new_left_node;
+		new_left_node->left = parent->left;
+		parent->left = new_left_node;
+	}
+	return (new_left_node);
+}
