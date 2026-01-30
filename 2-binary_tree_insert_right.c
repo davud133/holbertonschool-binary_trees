00797e63025d23d47fc3689f7dd60d18ce16b4d1
davud <davudgeroqiq@gmail.com>
2026-01-30 10:10:33 +0000

    new commit

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,22 +8,22 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_right_node;
+	binary_tree_t *new_right_node;
 
-        if (parent == NULL)
-                return (NULL);
-        new_right_node = binary_tree_node(parent, value);
-        if (new_right_node == NULL)
-                return (NULL);
-        if (parent->right == NULL)
-        {
-                parent->right = new_right_node;
-        }
-        else
-        {
-                parent->right->parent = new_right_node;
-                new_right_node->right = parent->right;
-                parent->right = new_right_node;
-        }
-        return (new_right_node);
+	if (parent == NULL)
+		return (NULL);
+	new_right_node = binary_tree_node(parent, value);
+	if (new_right_node == NULL)
+		return (NULL);
+	if (parent->right == NULL)
+	{
+		parent->right = new_right_node;
+	}
+	else
+	{
+		parent->right->parent = new_right_node;
+		new_right_node->right = parent->right;
+		parent->right = new_right_node;
+	}
+	return (new_right_node);
 }
