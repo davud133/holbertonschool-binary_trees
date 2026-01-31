74e5a493a270e2fb1b6774b8f2a187c807144ccd
davud <davudgeroqiq@gmail.com>
2026-02-01 02:16:11 +0300

    new commit

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,14 +9,15 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)
-		return(binary_tree_is_full(tree->left));
-	if (tree->right != NULL)
-		return(binary_tree_is_full(tree->right));
 	if (tree->right != NULL && tree->left != NULL)
 		return (1);
 	else if (tree->right == NULL && tree->left == NULL)
 		return (1);
 	else
 		return (0);
+	if (tree->left != NULL)
+		return(binary_tree_is_full(tree->left));
+	if (tree->right != NULL)
+		return(binary_tree_is_full(tree->right));
+
 }
