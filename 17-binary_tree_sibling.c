b68b26c05105052b68016f6aefe27146eeea10ae
davud <davudgeroqiq@gmail.com>
2026-02-01 02:41:59 +0300

    new commit

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,7 +10,16 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL && node->parent == NULL)
 		return (NULL);
 	if (node->parent->left == node)
-		return (node->parent->right);
+	{
+		if (node->parent->right != NULL)
+			return (node->parent->right);
+		return (NULL)
+	}
 	else
-		return (node->parent->left);
+	{
+		if (node->parent->left != NULL)
+			return (node->parent->left);
+		else
+			return (NULL);
+	}
 }
