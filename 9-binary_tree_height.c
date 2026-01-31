0bb48f0f47fce892cac4e18ba64309ebe835abea
davud <davudgeroqiq@gmail.com>
2026-01-31 21:12:18 +0300

    new commit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * bianry_tree_height - outputs the height of a tree
+ * binary_tree_height - outputs the height of a tree
  * @tree: binary tree
  *
  * Return: height of the binary tree
@@ -9,6 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0;
 	size_t right = 0;
+
 	if (tree != NULL)
 	{
 		if (tree->left == NULL && tree->right == NULL)
