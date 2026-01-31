20b237bce37e24daae92ba6476b715b86dc2fd55
davud <davudgeroqiq@gmail.com>
2026-01-31 22:30:17 +0300

    new commit

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: binary tree
+ *
+ * Return: count of nodes with at least 1 child
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t i = 0;
+	if (tree == NULL)
+	{
+		if (tree->left != NULL & tree->right != NULL)
+		{
+			i = 1;
+			if (tree->left != NULL)
+				i += binary_tree_nodes(tree->left);
+			if (tree->left != NULL)
+				i += binary_tree_nodes(tree->right);
+		}
+	}
+	return (i);
+}
