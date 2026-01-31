994ea4838a3c622cb4d20adf95415c8265b82b52
davud <davudgeroqiq@gmail.com>
2026-01-31 21:27:06 +0300

    new commit

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - gives the size of a binary tree
+ * @tree: binary tree
+ *
+ * Return: size of a binary tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t i = 0;
+	if (tree != NULL)
+	{
+		if (tree->left != NULL)
+			i = binary_tree_size(tree->left) + 1;
+		if (tree->right != NULL)
+			i = binary_tree_size(tree->right) + 1;
+	}
+	return (i);
