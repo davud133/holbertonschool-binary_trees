792fb7cc05b9bfdb07f0e0570b97112cd932ba41
davud <davudgeroqiq@gmail.com>
2026-01-31 22:30:40 +0300

    new commit

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,7 +10,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	size_t i = 0;
 	if (tree == NULL)
 	{
-		if (tree->left != NULL & tree->right != NULL)
+		if (tree->left != NULL && tree->right != NULL)
 		{
 			i = 1;
 			if (tree->left != NULL)
