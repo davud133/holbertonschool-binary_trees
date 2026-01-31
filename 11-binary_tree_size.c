3e5f8d90fde661d9652e2b503babb0acc4fa68d6
davud <davudgeroqiq@gmail.com>
2026-01-31 21:38:04 +0300

    new commit

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,7 +14,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		if (tree->left != NULL)
 			i += binary_tree_size(tree->left);
 		if (tree->right != NULL)
-			i += binary_tree_size(tree->right) + 1;
+			i += binary_tree_size(tree->right);
 	}
 	else
 		return (0);
