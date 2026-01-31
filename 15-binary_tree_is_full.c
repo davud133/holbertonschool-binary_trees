c0513075c2da9fa7a86e35bc38ac1d1f66ed9820
davud <davudgeroqiq@gmail.com>
2026-02-01 02:00:16 +0300

    new commit

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - check if a binary tree is full
+ * @tree: binary tree
+ *
+ * Return: 1 on success 0 on failure
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int i = 1;
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	if (tree->left != NULL)
+		i = binary_tree_is_full(tree->left);
+	if (tree->right != NULL)
+		i = binary_tree_is_full(tree->right);
+	return (i);
+}
