a72da468035f4b1b8818a08e009a83bae3779cba
davud <davudgeroqiq@gmail.com>
2026-01-31 21:09:49 +0300

    new commit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,6 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		if (tree->right != NULL)
 			right = binary_tree_height(tree->right);
 	}
+	else if (tree->left == NULL && tree->right == NULL)
+		return (1);
 	else
 		return (0);
 	if (left > right)
