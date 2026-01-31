07e1bf2c7210e5c56a773b982a5e5479d04024bd
davud <davudgeroqiq@gmail.com>
2026-01-31 23:13:31 +0300

    new commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,22 @@
 #include "binary_trees.h"
+int height(const binary_tree_t *tree)
+{
+	int left = 0;
+	int right = 0;
+	if (tree != NULL)
+	{
+		if (tree->left != NULL)
+			left = height(tree->left) + 1;
+		if (tree->right != NULL)
+			right = height(tree->right) + 1;
+	}
+	else
+		return (0);
+	if (left > right)
+		return  (left + 1);
+	else
+		return (right + 1);
+}
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: binary tree
@@ -7,16 +25,8 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = 0;
-	int right = 0;
-	if (tree != NULL)
-	{
-		if (tree->left != NULL)
-			left = binary_tree_balance(tree->left) + 1;
-		if (tree->right != NULL)
-			right = binary_tree_balance(tree->right) + 1;
-	}
-	else
+	if (tree == NULL)
 		return (0);
-	return (left - right);
+	int balance = height(tree->left) - height(tree->right);
+	return (balance);
 }
