54de116b4a3c16eed5259938acb7d07c2fab64de
davud <davudgeroqiq@gmail.com>
2026-02-01 21:14:13 +0300

    new commit

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,13 +10,9 @@ int height(const binary_tree_t *tree)
 	int left = 0;
 	int right = 0;
 	if (tree->left != NULL)
-	{
 		left = height(tree->left) + 1;
-	}
 	if (tree->right != NULL)
-	{
 		right = height(tree->right) + 1;
-	}
 	if (left > right)
 		return (left);
 	else
@@ -31,20 +27,22 @@ int height(const binary_tree_t *tree)
 int full(const binary_tree_t *tree)
 {
 	int i = 1;
+
+	if (tree == NULL)
+		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (i);
 	else if (tree->left != NULL && tree->right != NULL)
 	{
-		if (tree->left != NULL)
-			i = full(tree->left);
+		i = full(tree->left);
 		if (i == 0)
 			return (0);
-		if (tree->right != NULL)
-			i = full(tree->right);
+		i = full(tree->right);
+		return (i);
 	}
 	else
 		return (0);
-	return (i);
+	return (0);
 }
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -56,12 +54,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int left = 0;
 	int right = 0;
+
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)
-		left = height(tree->left);
-	if (tree->right != NULL)
-		right = height(tree->right);
+	left = height(tree->left);
+	right = height(tree->right);
+	if (left != right || full(tree) == 0)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	return ((left == right) && full(tree));
+	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 }
