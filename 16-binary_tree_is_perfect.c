0c694ea9b07d0a6d8c1cdc9d5b52aade92fa6e6f
davud <davudgeroqiq@gmail.com>
2026-02-01 21:15:39 +0300

    new commit

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -59,10 +59,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	left = height(tree->left);
 	right = height(tree->right);
-	if (left != right || full(tree) == 0)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	return ((left == right) && full(tree));
 }
